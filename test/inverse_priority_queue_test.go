66ef764c64a58b62bedd6e41a32ad644ad7bbd63
Chris Statkiewicz <christopherstat@protonmail.com>
2018-07-27 17:33:12 +0300

    Add inverse priority queue

--- test/inverse_priority_queue_test.go
+++ test/inverse_priority_queue_test.go
@@ -7,27 +7,27 @@ import (
 )
 
 // test helper function; initializes and adds size elements to the structure.
-func generateEqualQueue(size int) (queue gost.PriorityQueue) {
+func generateEqualInverseQueue(size int) (queue gost.InversePriorityQueue) {
 	for i := 0; i < size; i++ {
 		queue.Enqueue(newVector(i), 1)
 	}
 	return
 }
 
-func TestPriorityQueue_Len(t *testing.T) {
-	queue := gost.PriorityQueue{}
+func TestInversePriorityQueue_Len(t *testing.T) {
+	queue := gost.InversePriorityQueue{}
 	if queue.Size() != 0 {
 		t.Error("len() failed to return empty queue length")
 	}
 
-	queue = generateEqualQueue(10)
+	queue = generateEqualInverseQueue(10)
 	if queue.Size() != 10 {
 		t.Errorf("len() length: %v, expected: %v", queue.Size(), 10)
 	}
 }
 
-func TestPriorityQueue_Push(t *testing.T) {
-	queue := gost.NewPriorityQueue()
+func TestInversePriorityQueue_Push(t *testing.T) {
+	queue := gost.NewInversePriorityQueue()
 	queue.Enqueue("last", 0)
 	queue.Enqueue("middle", 1)
 	queue.Enqueue("first", 2)
@@ -37,16 +37,16 @@ func TestPriorityQueue_Push(t *testing.T) {
 	}
 }
 
-func TestPriorityQueue_Pop(t *testing.T) {
-	pq := gost.NewPriorityQueue()
+func TestInversePriorityQueue_Pop(t *testing.T) {
+	pq := gost.NewInversePriorityQueue()
 	pq.Enqueue("a", 0)
 	pq.Enqueue("b", 5)
 	pq.Enqueue("c", 10)
 	pq.Enqueue("d", 5)
 
 	value := pq.Dequeue().(string)
-	if value != "c" {
-		t.Errorf("Dequeue() failed: returned: %v, expected: %v", value, "c")
+	if value != "a" {
+		t.Errorf("Dequeue() failed: returned: %v, expected: %v", value, "a")
 	}
 
 	value = pq.Dequeue().(string)
@@ -60,16 +60,16 @@ func TestPriorityQueue_Pop(t *testing.T) {
 	}
 
 	value = pq.Dequeue().(string)
-	if value != "a" {
-		t.Errorf("Dequeue() failed: returned: %v, expected: %v", value, "a")
+	if value != "c" {
+		t.Errorf("Dequeue() failed: returned: %v, expected: %v", value, "c")
 	}
 
 	if pq.Size() != 0 {
-		t.Error("Dequeue() failed: PriorityQueue should be empty")
+		t.Error("Dequeue() failed: InversePriorityQueue should be empty")
 	}
 
 	null := pq.Dequeue()
 	if null != nil {
-		t.Error("Dequeue() failed: PriorityQueue returned non-nil value when empty")
+		t.Error("Dequeue() failed: InversePriorityQueue returned non-nil value when empty")
 	}
 }
